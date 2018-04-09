d1240b4a3652d22ef84380b6023cc4f1b8d7c695
michaelyangzz <michaelyangzz@msn.cn>
2018-04-09 22:18:43 +0800

    initial

---
+++ VGAGE.Doc/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿
+$(function () {
+    var cpath = window.location.pathname;
+    var $a;
+
+    $(".navbar-nav .nav-item a").each(function (idx, ele) {
+        if (ele.pathname === cpath) {
+            $a = $(ele);
+        }
+    });
+
+    if ($a && $a.hasClass("nav-link"))
+        $a.parent().addClass("active");
+    else if ($a && $a.hasClass("dropdown-item"))
+        $a.parent().parent().addClass("active");
+});
