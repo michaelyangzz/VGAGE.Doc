a5bc7534bbebe28a5ce4ed4760ad7753cc7842be
michaelyangzz <michaelyangzz@msn.cn>
2018-04-10 10:18:59 +0800

    hello

--- VGAGE.Doc/wwwroot/js/site.js
+++ VGAGE.Doc/wwwroot/js/site.js
@@ -1,16 +1,11 @@
-﻿
-$(function () {
+﻿$(function () {
     var cpath = window.location.pathname;
-    var $a;
-
-    $(".navbar-nav .nav-item a").each(function (idx, ele) {
-        if (ele.pathname === cpath) {
-            $a = $(ele);
-        }
-    });
-
-    if ($a && $a.hasClass("nav-link"))
-        $a.parent().addClass("active");
-    else if ($a && $a.hasClass("dropdown-item"))
-        $a.parent().parent().addClass("active");
+    var ta = (".navbar-nav .nav-item a[href='" + cpath + "']");
+    var $a = $(ta);
+    if ($a.length) {
+        if ($a.hasClass("nav-link"))
+            $a.parent().addClass("active");
+        else if ($a.hasClass("dropdown-item"))
+            $a.parent().parent().addClass("active");
+    }
 });
